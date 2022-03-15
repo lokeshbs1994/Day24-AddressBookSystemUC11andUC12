56a9392422c6c78f44b0b53d23cc07ee0c872e78
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-15 19:21:54 +0530

    ADDED: Sort Entries alphabetically by person's name

---
+++ src/main/java/com/bridgeLabz/AddressBook.java
@@ -0,0 +1,18 @@
+package com.bridgeLabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+
+}
